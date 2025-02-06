328ec1cc116c0399fd4fd32bb5b828d20d31a2d5
vynguyenn02 <vyhellen2112@gmail.com>
2025-02-06 22:38:57 +0700

    feat: banner, best seller

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+import Banner from "@/components/Banner/Banner";
+import BestSeller from "@/components/BestSeller/BestSeller";
+
 
 export default function Page() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 pt-20">{/* Nội dung trang chính */}</main>
+      <main className="flex-1 pt-20">
+        <Banner />
+        {/* Nội dung khác */}
+        <BestSeller></BestSeller>
+      </main>
       <Footer />
     </div>
   );
